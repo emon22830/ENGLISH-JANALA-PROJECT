d7f2ab7dce5e2ddd84c0b96917c703e562df206a
emon22830 <mdemon22830@gmail.com>
2025-09-05 00:38:52 +0800

    untill lesson api integrate completed

---
+++ script/index.js
@@ -0,0 +1,23 @@
+const loadLessons=()=>{
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then(res=>res.json())
+    .then((json) => displayLessons(json.data))
+};
+
+
+const displayLessons=(lessons)=>{
+    const levelContainer = document.getElementById("level-container");
+    levelContainer.innerHTML= " "
+    for(let lesson of lessons){
+        const btnDiv = document.createElement("div");
+        btnDiv.innerHTML=`
+        <button href="" class="btn btn-primary btn-outline"> <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>
+        `;
+
+        levelContainer.append(btnDiv);
+    }
+
+};
+loadLessons();
+
+
