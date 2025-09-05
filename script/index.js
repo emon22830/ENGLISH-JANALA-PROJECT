7374cb6e1ce174ca15d4863576c96c17be5946b5
emon22830 <mdemon22830@gmail.com>
2025-09-05 21:46:15 +0800

    Card API integration completed

--- script/index.js
+++ script/index.js
@@ -4,6 +4,37 @@ const loadLessons=()=>{
     .then((json) => displayLessons(json.data))
 };
 
+const loadLevelWord=(id)=>{
+    console.log(id);
+    const url = `https://openapi.programming-hero.com/api/levels/${id}`
+    console.log(url); 
+    fetch(url)
+    .then((res)=>res.json())
+    .then((data) =>displayLevelWord(data.data));
+};
+
+const displayLevelWord = (words)=>{
+   const wordContainer = document.getElementById("word-container");
+   wordContainer.innerHTML= "";
+
+   words.forEach((word) => {
+    console.log(word);
+    const card=document.createElement("div");
+    card.innerHTML=`
+     <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5">
+                <h2>Eager</h2>
+                <p>Meaning & Pronunciation</p>
+                <div>
+                    "আগ্রহী / ইগার"
+                </div>
+                </div>
+    `;
+    wordContainer.appendChild(card);
+   });  
+  
+};
+
+
 
 const displayLessons=(lessons)=>{
     const levelContainer = document.getElementById("level-container");
@@ -11,7 +42,7 @@ const displayLessons=(lessons)=>{
     for(let lesson of lessons){
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML=`
-        <button href="" class="btn btn-primary btn-outline"> <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>
+        <button onclick="loadLevelWord(${lesson.level_no})" href="" class="btn btn-primary btn-outline"> <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button>
         `;
 
         levelContainer.append(btnDiv);
